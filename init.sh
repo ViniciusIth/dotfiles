6fb3be42f9838f12ee88098191f674fe7ec316cb
ViniciusIth <vini.ithalo@hotmail.com>
2024-07-11 13:03:37 -0300

    fixed symlink on SRC when DEST already exists

--- init.sh
+++ init.sh
@@ -3,41 +3,47 @@
 # Determine the directory of the current script (dotfiles directory)
 DOTFILES_DIR=$(cd "$(dirname "$0")"; pwd)
 
-echo $DOTFILES_DIR
+echo "Dotfiles directory: $DOTFILES_DIR"
 
 # Function to create a symbolic link
 create_symlink() {
     local SRC=$1
     local DEST=$2
 
-    # Backup existing file or directory if it exists
+    # # Backup existing file or directory if it exists
+    # if [ -e "$DEST" ] || [ -L "$DEST" ]; then
+    #     echo "Backing up existing $DEST to $DEST.bak"
+    #     cp "$DEST" "$DEST.bak"
+    # fi
+
     if [ -e "$DEST" ] || [ -L "$DEST" ]; then
+        echo "$DEST already exist, skipping"
         return
-        # echo "Backing up existing $DEST to $SRC.bak"
-        # mv "$DEST" "$SRC.bak"
     fi
 
-    # Create the symbolic link
     ln -s "$SRC" "$DEST"
     echo "Created symlink: $SRC -> $DEST"
 }
 
 # Iterate over all files and directories in the dotfiles directory
 for FILE in "$DOTFILES_DIR"/.??* "$DOTFILES_DIR"/*; do
+    # Skip the dotfiles directory itself
+    if [ "$FILE" == "$DOTFILES_DIR" ]; then
+        continue
+    fi
+
     # Get the base name of the file or directory
     BASENAME=$(basename "$FILE")
 
-    echo $BASENAME
-    
-    # Skip init.sh and README.md file
+    # Skip init.sh and README.md files
     if [ "$BASENAME" == "init.sh" ] || [ "$BASENAME" == "README.md" ] || [ "$BASENAME" == ".git" ]; then
-	echo "skipping init and readme"
+        echo "Skipping init.sh, README.md, and .git"
         continue
     fi
 
     # Determine the destination path
     if [ "$BASENAME" == ".config" ]; then
-	    echo "found .config"
+        echo "Found .config directory"
         # Special handling for the .config directory
         for CONFIG_DIR in "$DOTFILES_DIR/.config"/*; do
             CONFIG_BASENAME=$(basename "$CONFIG_DIR")
